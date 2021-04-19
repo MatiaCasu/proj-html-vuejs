0ef265bd190266c5add3f55c316f99008eea93ee
Matia <matia.casu@gmail.com>
2021-04-19 18:47:22 +0200

    Aggiunta barra di link con menu a tendina visibili  all'hover

--- js/script.js
+++ js/script.js
@@ -7,6 +7,78 @@ const app = new Vue({
     // logo NavBar
 
     // nav menu-link
+    menuList:[
+      {
+        title: 'home',
+        linkList:[
+          {
+            nomeLink: 'prova1',
+            href: '#'
+          },
+          {
+            nomeLink: 'prova2',
+            href: '#'
+          },
+          {
+            nomeLink: 'Lorem ipsum dolor sit amet jnxcaslxdmaxp ksnax k-òmxC',
+            href: '#'
+          },
+        ]
+      },
+      {
+        title: 'pages',
+        linkList:[
+          {
+            nomeLink: 'prova1',
+            href: '#'
+          },
+          {
+            nomeLink: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolo',
+            href: '#'
+          }
+        ]
+      },
+      {
+        title: 'courses',
+        linkList:[
+          {
+            nomeLink: 'prova1',
+            href: '#'
+          },
+          {
+            nomeLink: 'prova2',
+            href: '#'
+          }
+        ]
+      },
+      {
+        title: 'features',
+        linkList:[
+          {
+            nomeLink: 'prova1',
+            href: '#'
+          }
+        ]
+      },
+      {
+        title: 'blog',
+        linkList:[
+          {
+            nomeLink: 'prova1',
+            href: '#'
+          }
+        ]
+      },
+      {
+        title: 'shop',
+        linkList:[
+          {
+            nomeLink: 'prova1',
+            href: '#'
+          }
+        ]
+      },
+    ],
     // /nav menu-link
 
   }
