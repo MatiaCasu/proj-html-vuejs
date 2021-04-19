f060dc088094fa4927bdafe64b912ea9d22404cc
Matia <matia.casu@gmail.com>
2021-04-19 14:57:08 +0200

    Struttura generale navbar e allineamento logo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    // logo NavBar
+    logoSrc: "img/dark-logo.png",
+    altLogo: "Max Coach logo",
+    // logo NavBar
+
+    // nav menu-link
+    // /nav menu-link
+
+  }
+});
