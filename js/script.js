19628fea63008b5aeb409c4372bd64f95c06b490
Matia <matia.casu@gmail.com>
2021-04-21 16:12:09 +0200

    Aggiunti dati template tramite Vue in lista corsi

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const app = new Vue({
   el: "#app",
   data: {
-    
+
     // header NAV BAR
     // logo NavBar
     logoSrc: "img/dark-logo.png",
@@ -113,9 +113,145 @@ const app = new Vue({
     ],
     // HERO partners logo
 
+    // featured courses link/info
+    featuredCourses: [
+      {
+        href: "https://www.google.it/",
+        img: "img/course-photo1.jpg",
+        price: {
+          int: "$40",
+          float: ".00"
+        },
+        name: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        others: [
+          {
+            icon: "far fa-file-alt",
+            type: "Lessons",
+            value: 3
+          },
+          {
+            icon: "far fa-user",
+            type: "Students",
+            value: 10
+          },
+          {
+            icon: "far fa-flag",
+            type: "Language",
+            value: "En"
+          },
+        ]
+      },
+      {
+        href: "#",
+        img: "img/stock-full-hd-03-480x298.jpg",
+        price: {
+          int: "Free",
+          float: ""
+        },
+        name: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ",
+        others: [
+          {
+            icon: "far fa-file-alt",
+            type: "Lessons",
+            value: 3
+          },
+          {
+            icon: "far fa-user",
+            type: "Students",
+            value: 10
+          }
+        ]
+      },
+      {
+        href: "#",
+        img: "img/stock-full-hd-04-480x298.jpg",
+        price: {
+          int: "$19",
+          float: ".00"
+        },
+        name: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ",
+        others: [
+          {
+            icon: "far fa-file-alt",
+            type: "Lessons",
+            value: 3
+          },
+          {
+            icon: "far fa-user",
+            type: "Students",
+            value: 10
+          }
+        ]
+      },
+      {
+        href: "#",
+        img: "img/stock-full-hd-05-480x298.jpg",
+        price: {
+          int: "$26",
+          float: ".00"
+        },
+        name: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ",
+        others: [
+          {
+            icon: "far fa-file-alt",
+            type: "Lessons",
+            value: 3
+          },
+          {
+            icon: "far fa-user",
+            type: "Students",
+            value: 10
+          }
+        ]
+      },
+      {
+        href: "#",
+        img: "img/stock-full-hd-06-480x298.jpg",
+        price: {
+          int: "$39",
+          float: ".00"
+        },
+        name: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+        others: [
+          {
+            icon: "far fa-file-alt",
+            type: "Lessons",
+            value: 3
+          },
+          {
+            icon: "far fa-user",
+            type: "Students",
+            value: 10
+          }
+        ]
+      },
+      {
+        href: "#",
+        img: "img/course-featured-image-01-480x298.jpg",
+        price: {
+          int: "$59",
+          float: ".00"
+        },
+        name: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+        others: [
+          {
+            icon: "far fa-file-alt",
+            type: "Lessons",
+            value: 3
+          },
+          {
+            icon: "far fa-user",
+            type: "Students",
+            value: 10
+          }
+        ]
+      }
+    ],
+    // featured courses link/info
+
     // FOOTER Lists
     // /footer EXPLORE
-    exploreLink: [
+    footerExploreLink: [
       {
         nameLink: 'explore',
         href: '#'
@@ -144,7 +280,7 @@ const app = new Vue({
     // /footer EXPLORE
 
     // footer INFORMATIONS
-    infoLink: [
+    footerInfoLink: [
       {
         nameLink: 'info',
         href: '#'
@@ -164,8 +300,5 @@ const app = new Vue({
     ]
     // /footer INFORMATIONS
     // /FOOTER Lists
-
-
-
   }
 });
